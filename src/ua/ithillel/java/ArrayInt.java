84f5e140504e41016b0b5a72cb96a787f4eb91a5
VyacheslavSibov <105800498+VyacheslavSibov@users.noreply.github.com>
2022-07-02 09:30:50 +0300

    Update ArrayInt.java

--- src/ua/ithillel/java/ArrayInt.java
+++ src/ua/ithillel/java/ArrayInt.java
@@ -3,19 +3,17 @@ package ua.ithillel.java;
 public class ArrayInt {
     public static void main(String[] args) {
         // ctrl + alt + L
-        int[][] score = new int[3][4];
-        score[0][0] = 10;
-        score[0][1] = 11;
-        score[0][2] = 12;
-        score[0][3] = 13;
-        score[1][0] = 20;
-        score[1][1] = 21;
-        score[1][2] = 22;
-        score[1][3] = 23;
-        score[2][0] = 30;
-        score[2][1] = 31;
-        score[2][2] = 32;
-        score[2][3] = 33;
+        int xSize = 3;
+        int ySize = 4;
+
+        int[][] score = new int[xSize][ySize];
+        for (int i = 0; i < score.length; i++) {
+            for (int j = 0; j < score[i].length; j++) {
+                score[i][j] = 10 * (i + 1) + j;
+
+            }
+
+        }
         System.out.println("[");
         for (int i = 0; i < score.length; i++) {
             System.out.print("   [");
